c51e1398065a4e2021ddfe442ad67ffc84b8013d
Mittensdlara <pooraghaeedelara@gmail.com>
2023-01-04 20:39:56 +0330

    Fixed the tabs

---
+++ src/Todos.js
@@ -0,0 +1,34 @@
+ export const todos = [
+      {
+        id: 0,
+        title: "First todo (today)",
+        date: new Date("2022/11/16 21:29"),
+      },
+      {
+        id: 1,
+        title: "Second todo (today)",
+        date: new Date("1997/06/14 9:18"),
+      },
+      {
+        id: 2,
+        title: "Third todo (today)",
+        date: new Date("2006/08/03 16:44"),
+      },
+
+      {
+        id: 0,
+        title: "First todo (important)",
+        date: new Date("2022/11/16 21:29")
+      },
+      {
+        id: 1,
+        title: "Second todo (important)",
+        date: new Date("1997/06/14 9:18")
+      },
+      {
+        id: 2,
+        title: "Third todo (important)",
+        date: new Date("2006/08/03 16:44")
+      }
+    
+    ];
---
+++ src/TodosData.js
@@ -0,0 +1,38 @@
+const todosData = {
+    today: [
+      {
+        id: 0,
+        title: "First todo (today)",
+        date: "2022/11/16 21:29"
+      },
+      {
+        id: 1,
+        title: "Second todo (today)",
+        date: "1997/06/14 9:18"
+      },
+      {
+        id: 2,
+        title: "Third todo (today)",
+        date: "2006/08/03 16:44"
+      }
+    ],
+    important: [
+      {
+        id: 0,
+        title: "First todo (important)",
+        date: "2022/11/16 21:29"
+      },
+      {
+        id: 1,
+        title: "Second todo (important)",
+        date: "1997/06/14 9:18"
+      },
+      {
+        id: 2,
+        title: "Third todo (important)",
+        date: "2006/08/03 16:44"
+      }
+    ]
+  };
+
+  export default todosData
